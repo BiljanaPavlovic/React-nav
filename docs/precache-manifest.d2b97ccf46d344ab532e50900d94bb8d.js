2c60510fefed8d812c918c00197c559eb67fbf92
Biljana <zabiljanupavlovic@gmail.com>
2021-03-15 23:55:50 +0100

    docs folder added

---
+++ docs/precache-manifest.d2b97ccf46d344ab532e50900d94bb8d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637f539fd3805779f3a0bd84db461966",
+    "url": "/React-nav/index.html"
+  },
+  {
+    "revision": "60507dcb7a4631c53acc",
+    "url": "/React-nav/static/css/main.0553d329.chunk.css"
+  },
+  {
+    "revision": "1b397f1d5896cefda701",
+    "url": "/React-nav/static/js/2.5dba0b8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-nav/static/js/2.5dba0b8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60507dcb7a4631c53acc",
+    "url": "/React-nav/static/js/main.d83bcc93.chunk.js"
+  },
+  {
+    "revision": "f0d25e0c5d9d29d30430",
+    "url": "/React-nav/static/js/runtime-main.98917ea1.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/React-nav/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "060ed2ed1085988df599b6e403350fb1",
+    "url": "/React-nav/static/media/bell.060ed2ed.svg"
+  },
+  {
+    "revision": "681afa2ed6c043ea96751e7b755cc561",
+    "url": "/React-nav/static/media/bolt.681afa2e.svg"
+  },
+  {
+    "revision": "3660abd00cc9808e49523ee7f0135f49",
+    "url": "/React-nav/static/media/caret.3660abd0.svg"
+  },
+  {
+    "revision": "efbb2bd6e6e3066e53de373d26c1e039",
+    "url": "/React-nav/static/media/chevron.efbb2bd6.svg"
+  },
+  {
+    "revision": "993f0cc0e8de3fe0490e28fad0ee23a3",
+    "url": "/React-nav/static/media/cog.993f0cc0.svg"
+  },
+  {
+    "revision": "b183a44a8a6afe740017339f7fc813ee",
+    "url": "/React-nav/static/media/messenger.b183a44a.svg"
+  },
+  {
+    "revision": "449172897c0f4485afa6d339dd4170b0",
+    "url": "/React-nav/static/media/plus.44917289.svg"
+  }
+]);
